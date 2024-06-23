079f7e30cf7e84761d1831211f3245c31e4b401c
Threshika V S <115970544+Threshik@users.noreply.github.com>
2024-06-23 11:44:31 +0530

    Create Buy_and_Sell_Crypto.java

---
+++ Buy_and_Sell_Crypto.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int res=0;
+        for(int i=0;i<prices.length;i++)
+        {
+            if(prices[i]<min)
+            {
+                min = prices[i];
+            }
+            res = Math.max(res,prices[i]-min);
+        }
+        return res;
+    }
+}

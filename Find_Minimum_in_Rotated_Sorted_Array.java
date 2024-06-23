8134458cf47a8c932b31e063e96bd28ce87327c2
Threshika V S <115970544+Threshik@users.noreply.github.com>
2024-06-23 13:53:57 +0530

    Create Find_Minimum_in_Rotated_Sorted_Array.java

---
+++ Find_Minimum_in_Rotated_Sorted_Array.java
@@ -0,0 +1,20 @@
+//using binary search for this instead of sorting and finding the min
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left<right)
+        {
+            int mid = (left+right)/2;
+            if(nums[mid]<nums[right])
+            {
+                right = mid;
+            }
+            else
+            {
+                left=mid+1;
+            }
+        }
+        return nums[left];
+    }
+}

d69918a4d0302ce1dbf74381bc7f9012ab8786fb
Threshika V S <115970544+Threshik@users.noreply.github.com>
2024-08-09 15:02:58 +0530

    Create Merge K Sorted Linked Lists.java

---
+++ Merge K Sorted Linked Lists.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0;i<lists.length;i++)
+        {
+           ListNode temp = lists[i];
+           while(temp!=null)
+           {
+            pq.add(temp.val);
+            temp=temp.next;
+           }
+        }
+        ListNode dn = new ListNode(-1);
+        ListNode t = dn;
+        while(pq.size()!=0)
+        {
+            int nn = pq.poll();
+            t.next=new ListNode(nn);
+            t=t.next;
+        }
+        return dn.next;
+
+    }
+}
